bacbc8d0c42630dbb19e7c06ac574d5f256894cb
Olshanskaya <viktoriarubenovna@gmail.com>
2023-06-11 11:33:23 +0300

    add substance page

--- src/AddMedicinePage.js
+++ src/AddMedicinePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Container, Form, InputGroup } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Button, Container, Form, Modal } from 'react-bootstrap';
 import Select from 'react-select';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const AddMedicinePage = () => {
@@ -11,6 +11,10 @@ const AddMedicinePage = () => {
   const [medicineName, setMedicineName] = useState('');
   const [isCreateButtonDisabled, setIsCreateButtonDisabled] = useState(true);
   const [createStatus, setCreateStatus] = useState('');
+  const [editedMedicineId, setEditedMedicineId] = useState(null);
+  const [editedMedicineName, setEditedMedicineName] = useState(''); // Состояние изменяемого названия лекарства
+  const [editedActiveSubstance, setEditedActiveSubstance] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,9 +24,13 @@ const AddMedicinePage = () => {
 
   const fetchMedicines = () => {
     try {
-      const response = fetch('http://localhost:8080/admin/meds/all');
-      const data = response.json();
-      setMedicines(data);
+      fetch('http://localhost:8080/admin/meds/all')
+        .then(response => response.json())
+        .then(data => {
+          const sortedMedicines = data.sort((a, b) => a.name.localeCompare(b.name)); // Сортировка по алфавиту
+          setMedicines(sortedMedicines);
+        })
+        .catch(error => console.error('Error fetching medicines:', error));
     } catch (error) {
       console.error('Error fetching medicines:', error);
     }
@@ -30,22 +38,25 @@ const AddMedicinePage = () => {
 
   const fetchActiveSubstances = () => {
     try {
-      const response = fetch('http://localhost:8080/admin/sub/all');
-      const data = response.json();
-      const sortedSubstances = data
-        .map((substance) => ({ value: substance.name, label: substance.name }))
-        .sort((a, b) => a.label.localeCompare(b.label)); // Сортировка по алфавиту
-      setActiveSubstances(sortedSubstances);
+      fetch('http://localhost:8080/admin/sub/all')
+        .then(response => response.json())
+        .then(data => {
+          const sortedSubstances = data
+            .map(substance => ({ value: substance.id, label: substance.name }))
+            .sort((a, b) => a.label.localeCompare(b.label)); // Сортировка по алфавиту
+          setActiveSubstances(sortedSubstances);
+        })
+        .catch(error => console.error('Error fetching active substances:', error));
     } catch (error) {
       console.error('Error fetching active substances:', error);
     }
   };
 
-  const handleSubstanceChange = (selectedOption) => {
+  const handleSubstanceChange = selectedOption => {
     setSelectedSubstance(selectedOption);
   };
 
-  const handleMedicineNameChange = (event) => {
+  const handleMedicineNameChange = event => {
     setMedicineName(event.target.value);
   };
 
@@ -58,35 +69,92 @@ const AddMedicinePage = () => {
   };
 
   const handleCreateButtonClick = () => {
-    const confirmCreate = window.confirm(`Вы уверены, что хотите создать лекарственное средство?`);
+    const confirmCreate = window.confirm('Вы уверены, что хотите создать лекарственное средство?');
     if (confirmCreate) {
       const data = {
-        email: 'asdlefnekjfn',
-        password: '123',
-        authority: {
-          id: 2,
-          authority: 'ADMIN'
-        }
+        name: medicineName,
+        activeSubstanceId: selectedSubstance.value
       };
 
-      axios.post('http://localhost:8080/register', data)
+      axios
+        .post('http://localhost:8080/admin/meds/new', data)
         .then(response => {
-          // Обработка успешного создания администратора
-          window.alert(`Администратор успешно создан.`);
-          //window.location.reload();
+          if (response && response.data && response.data.id !== null) {
+            window.alert('Лекарственное средство создано');
+            // Очистить поля и обновить список лекарственных средств
+            setMedicineName('');
+            setSelectedSubstance(null);
+            fetchMedicines();
+          } else {
+            window.alert('Такое лекарственное средство уже существует');
+          }
         })
         .catch(error => {
-          // Обработка ошибки при создании администратора
-          console.error(error);
-          window.alert('Ошибка при создании администратора.');
+          console.error('Ошибка при создании лекарственного средства:', error);
+          window.alert('Ошибка при создании лекарственного средства.');
         });
     }
   };
 
+  const handleDeleteButtonClick = medicineId => {
+    console.log('Delete Medicine ID:', medicineId);
+    // Отправка GET-запроса на удаление лекарственного средства
+    axios
+      .get(`http://localhost:8080/admin/meds/delete/${medicineId}`)
+      .then(response => {
+        if (response.status === 200) {
+          window.alert('Лекарственное средство удалено успешно');
+          window.location.reload();
+        } else {
+          window.alert('Ошибка при удалении лекарственного средства');
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting medicine:', error);
+        window.alert('Ошибка при удалении лекарственного средства');
+      });
+  };
+
+  const handleEditButtonClick = (medicine) => {
+    setEditedMedicineId(medicine.id);
+    console.log('Edit Medicine ID:', medicine.id);
+    setEditedMedicineName(medicine.name);
+    setEditedActiveSubstance(activeSubstances.find((substance) => substance.label === medicine.activeSubstanceDto.name));
+    setIsEditModalOpen(true);
+  };
+
+  const handleSaveEdit = () => {
+    console.log('Изменение лекарственного средства');
+
+    const editedData = {
+      name: editedMedicineName,
+      activeSubstanceId: editedActiveSubstance.value,
+    };
+
+    axios
+      .put(`http://localhost:8080/admin/meds/edit/${editedMedicineId}`, editedData)
+      .then((response) => {
+        console.log('Лекарственное средство успешно изменено');
+        window.alert('Название измененео успешно');
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Ошибка при изменении лекарственного средства:', error);
+      });
+
+    setIsEditModalOpen(false); // Закрытие модального окна
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditModalOpen(false); // Закрытие модального окна
+  };
+
+  const handleEditActiveSubstanceChange = (selectedOption) => {
+    setEditedActiveSubstance(selectedOption);
+  };
 
   return (
     <Container>
-
       <div className="d-flex justify-content-end mb-3">
         <Button variant="primary" className="me-2" onClick={handleGoBack}>
           Назад
@@ -114,22 +182,63 @@ const AddMedicinePage = () => {
         </Form.Group>
 
         <Button variant="primary" type="submit" style={{ marginTop: '10px' }} onClick={handleCreateButtonClick} disabled={isCreateButtonDisabled}>
-            Создать
+          Создать
         </Button>
       </Form>
 
       <h2>Все лекарственные средства</h2>
-      {medicines.map((medicine) => (
+      {medicines.map(medicine => (
         <div key={medicine.id}>
           {`${medicine.name} (${medicine.activeSubstanceDto.name})`}
           <div style={{ marginLeft: '10px' }}>
-            <Button variant="info">Редактировать</Button>
-            <Button variant="danger">Удалить</Button>
+            <Button variant="info" onClick={() => handleEditButtonClick(medicine)}>
+              Редактировать
+            </Button>
+            <Button variant="danger" onClick={() => handleDeleteButtonClick(medicine.id)}>
+              Удалить
+            </Button>
           </div>
         </div>
       ))}
+
+     {/* Модальное окно редактирования */}
+     <Modal show={isEditModalOpen} onHide={handleCancelEdit}>
+        <Modal.Header closeButton>
+          <Modal.Title>Редактировать лекарственное средство</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group controlId="editMedicineName">
+            <Form.Label>Название:</Form.Label>
+            <Form.Control
+              type="text"
+              value={editedMedicineName}
+              onChange={(event) => setEditedMedicineName(event.target.value)}
+            />
+          </Form.Group>
+
+          {/* <Form.Group controlId="editActiveSubstance">
+            <Form.Label>Активное вещество:</Form.Label>
+            <Select
+              options={activeSubstances}
+              value={activeSubstances.find((substance) => substance.label === editedActiveSubstance)}
+              onChange={(selectedOption) => handleEditActiveSubstanceChange(selectedOption)}
+              isClearable
+              isSearchable
+            />
+          </Form.Group> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setIsEditModalOpen(false)}>
+            Отменить
+          </Button>
+          <Button variant="primary" onClick={handleSaveEdit}>
+            Сохранить
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </Container>
   );
-}
+};
 
 export default AddMedicinePage;
--- src/AddSubstancePage.js
+++ src/AddSubstancePage.js
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Form, Modal } from 'react-bootstrap';
+import axios from 'axios';
 
-function AddSubstancePage() {
+const AddSubstancePage = () => {
+  const [substances, setSubstances] = useState([]);
+  const [newSubstance, setNewSubstance] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // Отправка GET-запроса для получения списка веществ
+    axios.get('http://localhost:8080/admin/sub/all')
+      .then(response => setSubstances(response.data))
+      .catch(error => console.error('Ошибка:', error));
+  }, []);
+
+  const handleSave = () => {
+    // Отправка POST-запроса для сохранения нового вещества
+    axios.post('http://localhost:8080/admin/sub', { name: newSubstance })
+      .then(response => {
+        // Обновление списка веществ после успешного сохранения
+        setSubstances([...substances, response.data]);
+        setNewSubstance('');
+      })
+      .catch(error => console.error('Ошибка:', error));
+  };
+
+  const handleDelete = (id) => {
+    // Отправка DELETE-запроса для удаления вещества
+    axios.delete(`http://localhost:8080/admin/sub/${id}`)
+      .then(() => {
+        // Обновление списка веществ после успешного удаления
+        setSubstances(substances.filter(substance => substance.id !== id));
+      })
+      .catch(error => console.error('Ошибка:', error));
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div>
+    <Container>
       <h1>Внести новое действующее вещество</h1>
-      {/* Add the content for the Add Substance page */}
-    </div>
+
+      <Form.Group>
+        <Form.Label>Название нового действующего вещества:</Form.Label>
+        <Form.Control
+          type="text"
+          value={newSubstance}
+          onChange={event => setNewSubstance(event.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" onClick={handleSave}>Сохранить</Button>
+
+      <h2>Существующие действующие вещества</h2>
+
+      <ul>
+        {substances.map(substance => (
+          <li key={substance.id}>
+            <div>
+              {substance.name}
+            </div>
+            <div>
+              <Button variant="secondary">Добавить взаимодействия</Button>
+              <Button variant="info">Редактировать</Button>
+              <Button variant="danger" onClick={() => handleDelete(substance.id)}>Удалить</Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Добавить взаимодействия</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Содержимое модального окна для добавления взаимодействий */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>Закрыть</Button>
+          <Button variant="primary">Сохранить</Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
   );
 }
 
